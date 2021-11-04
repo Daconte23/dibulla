75373831402fb0f32c6e9a3da8770d8fbfc75c70
Elliot <89425186+Stiven002@users.noreply.github.com>
2021-11-04 17:37:47 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const imagen1 = document.getElementById('imagen1');
+const imagen2 = document.getElementById('imagen2');
+
+const cargarimagen = (entradas, onbservador) => {
+    
+    entradas.forEach((entrada) => {
+        if(entrada.isIntersecting){
+            entrada.target.classList.add('visible');
+        }
+    });
+}
+
+const onbservador = new IntersectionObserver (cargarimagen,{
+    root: null,
+    rootMargin: '0px 0px 0px 0px',
+    threshold: 0.3
+} );
+
+onbservador.observe(imagen1);
+onbservador.observe(imagen2);
