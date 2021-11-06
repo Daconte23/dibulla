1b980f5a7d2d39f7b608d38fe9750f56b53f6d09
NeNeon09 <89425059+NeNeon09@users.noreply.github.com>
2021-11-06 02:58:10 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,28 @@ const onbservador = new IntersectionObserver (cargarimagen,{
 
 onbservador.observe(imagen1);
 onbservador.observe(imagen2);
+
+const parrafo1 = document.getElementById('parrafo1');
+const parrafo2 = document.getElementById('parrafo2');
+const parrafo3 = document.getElementById('parrafo3');
+const parrafo4 = document.getElementById('parrafo4');
+
+const cargarimagen = (entradas, onbservador) => {
+    
+    entradas.forEach((entrada) => {
+        if(entrada.isIntersecting){
+            entrada.target.classList.add('visible');
+        }
+    });
+}
+
+const onbservador = new IntersectionObserver (cargarimagen,{
+    root: null,
+    rootMargin: '0px 0px 0px 0px',
+    threshold: 0.3
+} );
+
+onbservador.observe(parrafo1);
+onbservador.observe(parrafo2);
+onbservador.observe(parrafo3);
+onbservador.observe(parrafo4);
