960427b61d8ade696f4637d6f2cc25383f971645
Elliot <89425186+Stiven002@users.noreply.github.com>
2021-11-19 14:31:02 -0500

    Add files via upload

---
+++ paginas/js/hoteles.js
@@ -0,0 +1,15 @@
+const typed = new Typed('.typed', {
+    strings: [
+        '<i class="text2">Descanso</i>',
+        '<i class="text2">Diversion</i>', 
+        '<i class="text2">Entretenimiento</i>'],
+
+        typeSpeed: 60,
+        startDelay: 300,
+        backSpeed: 60,
+        shuffle: false,
+        backDelay: 1000,
+        loop: true,
+        loopCount: false,
+        showCursor: true,
+});
